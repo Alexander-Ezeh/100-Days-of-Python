635d1b60555de44ea7a07441f3b6aeba3988fa45
Alexander-Ezeh <alexanderikennaefunaezeh@gmail.com>
2024-04-20 05:07:39 +0100

    100 days of Python started

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+# first test assignment using and understanding print function
 print("Day 1 - Python Print Function")
-print("The function is decleared like this:")
+print("The function is declared like this:")
 print("print('what to print')")
 
+# printing hello would and duplicating it using \n
+print("Hello World! \n Hello World!")
+
+# using the print and input function and string concatenation
+print("Hello " + input("What is your name?"))
