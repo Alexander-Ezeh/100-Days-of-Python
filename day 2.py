14f4a7cb44e6bd76caee3fd01bf8368343a1c75d
Alexander-Ezeh <alexanderikennaefunaezeh@gmail.com>
2024-04-22 19:24:27 +0100

    100 days of Python started

---
+++ day 2.py
@@ -0,0 +1,64 @@
+#num_char = len(input("What is your name? "))
+
+#new_num_char = str(num_char)
+
+#print("Your name has " + new_num_char + " Characters.")
+
+# 🚨 Don't change the code below 👇
+#two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+#first_digit = two_digit_number[0]
+#second_digit = two_digit_number[1]
+
+#result = int(first_digit) + int(second_digit)
+
+#print(result)
+
+#print (3 * (3 + 3) / 3 - 3)
+
+# 🚨 Don't change the code below 👇
+#height = input("enter your height in m: ")
+#weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+#bmi = float(weight) / float(height) ** 2
+#bmi_as_int = int(bmi)
+#print(bmi_as_int)
+
+#f-string
+
+# 🚨 Don't change the code below 👇
+# age = input("What is your current age?")
+# # 🚨 Don't change the code above 👆
+#
+# #Write your code below this line 👇
+#
+# age_as_int = int(age)
+#
+# years_remaining = 90 - age_as_int
+#
+# days_remaining = years_remaining * 365
+#
+# weeks_remaining = years_remaining * 52
+#
+# months_remaining = years_remaining * 12
+#
+# message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left."
+#
+# print(message)
+
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-# first test assignment using and understanding print function
+# # first test assignment using and understanding print function
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
-
-# printing hello would and duplicating it using \n
+#
+# # printing hello would and duplicating it using \n
 print("Hello World! \n Hello World!")
-
+#
 # using the print and input function and string concatenation
 print("Hello " + input("What is your name?"))
+
+#switching variables
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
+
+#Building a Band Name generator using location and pet name.
+#welcome message
+print("Welcome to Band Name Generator")
+#Location where user grew up
+location = input("Where did you grow up? \n")
+#childhood name for pet
+pet_name = input("What was the name of your childhood pet? \n")
+#Band Name Generated
+print("Your band name is: \n" + location + " " + pet_name)
